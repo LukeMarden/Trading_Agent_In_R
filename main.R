6af42b7f81b62b083ad59c8c25cd3d6c1c9da536
LukeMarden <55838911+LukeMarden@users.noreply.github.com>
2022-09-15 20:05:38 +0100

    Start

---
+++ main.R
@@ -0,0 +1,10 @@
+library(readr)
+library(TTR)
+library(quantmod)
+
+av <- read_csv("AV.L.csv")
+summary(av)
+sum(is.na(av))
+
+av$sma <- SMA(av[,"Close"], n=20)
+av$bbands <- BBands( av[,c("High","Low","Close")] )
